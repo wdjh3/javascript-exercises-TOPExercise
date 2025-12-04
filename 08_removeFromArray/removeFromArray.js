83a4312e8dd50fd88514bd19b11cbb85e49ff737
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2025-12-04 18:55:22 +0800

    Add solution to 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removed) {
+    let result = array;
+    for (const removedItem of removed) {
+        result = result.filter((item) => item !== removedItem);
+    }
+    return result;
 };
 
 // Do not edit below this line
