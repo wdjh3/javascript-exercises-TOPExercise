5a9befe06bca150b03360f4f37e706266859e238
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2025-12-04 18:22:20 +0800

    Add solution for 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = "";
+    for (let i = string.length - 1 ; i >= 0 ; i--) {
+        result = result += string.charAt(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
