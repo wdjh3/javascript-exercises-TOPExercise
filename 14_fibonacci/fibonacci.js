f8b12eb54df4de23865f574f1907ca5915ad32cb
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2025-12-07 17:39:38 +0800

    Add solution to 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(order) {
+    if (order < 0) {
+        return "OOPS";
+    }
+    order = parseInt(order);
+    let array = [0, 1];
+    for (let i = 0 ; i < order ; i++) {
+        let nextNumber = array[0] + array[1];
+        array[0] = array[1];
+        array[1] = nextNumber;
+    }
+    return array[0];
 };
 
 // Do not edit below this line
