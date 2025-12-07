ac488214ddef1f0ae3ba4f87c5db2f040070c799
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2025-12-07 18:08:49 +0800

    Add solution to 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
+    return array.reduce((currentPerson, nextPerson) => {
+        if (!currentPerson.yearOfDeath) {
+            currentPerson.yearOfDeath = currentYear;
+        }
 
+        if (!nextPerson.yearOfDeath) {
+            nextPerson.yearOfDeath = currentYear;
+        }
+        
+
+        const currentPersonYears = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        const nextPersonYears = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+
+        return (currentPersonYears > nextPersonYears ? currentPerson : nextPerson);
+    })
 };
 
 // Do not edit below this line
