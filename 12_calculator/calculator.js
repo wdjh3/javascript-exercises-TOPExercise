28b7d7adce6b6a0a658290b9f59368b502c270bf
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2025-12-07 17:14:00 +0800

    Add solution for 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(((acc, curr) => acc += curr), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(((acc, curr) => acc *= curr), 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  result = 1;
+	for (let i = 0 ; i < b ; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0 || a === 1) {
+    return 1;
+  }
+  result = 1;
+	for (let i = 2 ; i <= a ; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
