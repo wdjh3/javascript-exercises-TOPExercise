4f0db07c83063308818251ded5fe73516592008f
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2025-12-07 17:27:30 +0800

    Add solution to 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let array = string.split("");
+    let filteredArray = array.filter(char => {
+        return char !== " " && 
+        char !== "," && 
+        char !== "." && 
+        char !== "!" && 
+        char !== "?"
+    })
+    console.log(array + filteredArray);
+    return (filteredArray.join('') === filteredArray.reverse().join(''));
 };
 
 // Do not edit below this line
