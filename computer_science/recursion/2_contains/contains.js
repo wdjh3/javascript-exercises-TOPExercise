7bc1272390ec1b71c6dd0bb33242a892c1533270
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-23 17:24:39 +0800

    Add solution to 2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,24 @@
-const contains = function() {
-  
+const contains = function (object, searchItem) {
+  for (const key in object) {
+    const searchedItem = object[key];
+    if (
+      searchedItem === searchItem ||
+      (Number.isNaN(searchedItem) && Number.isNaN(searchItem))
+    ) {
+      return true;
+    } else {
+      console.log(searchedItem);
+      if (typeof searchedItem === "object") {
+        if (!contains(searchedItem, searchItem)) {
+          continue;
+        } else {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
