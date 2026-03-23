aa38a04fb90cdc45b6a873f9573b838aa49cff8f
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-23 16:27:28 +0800

    Add solution to 1_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,15 @@
-const factorial = function() {
+const factorial = function(n) {
+    if (typeof n !== "number" || Math.round(n) !== n || n < 0) {
+        return undefined;
+    }
 
+    if (n === 0) {
+        return 1;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 };
 
 // Do not edit below this line
