222ebcf36e6e2706f286065e6c400f6d3f7be58f
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-24 12:25:28 +0800

    Add solution to 3_totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,27 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (nestedArray) {
+  if (typeof nestedArray !== "object") {
+    return undefined;
+  }
+
+  let integerCount = 0;
+
+  function countIntegers(nestedArray) {
+    for (const key in nestedArray) {
+      const value = nestedArray[key];
+      if (Number.isInteger(value)) {
+        integerCount++;
+      } else if (typeof value === "object") {
+        countIntegers(value);
+      }
+    }
+  }
+
+  countIntegers(nestedArray);
+
+  return integerCount;
 };
-  
+
+console.log(totalIntegers([4, 6, { a: 1, b: { a: [5, 10], b: 11 } }, 9]));
+
 // Do not edit below this line
 module.exports = totalIntegers;
