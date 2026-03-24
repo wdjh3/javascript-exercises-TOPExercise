4b9d5ab9de94892cf40f0d0525613b190143bb57
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-24 13:38:27 +0800

    Add solution to 5_pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,15 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) {
+    return [1];
+  } else {
+    let array = [0].concat(pascal(n - 1)).concat([0]);
+    for (let i = 0; i < array.length - 1; i++) {
+        array[i] = array[i] + array[i + 1];
+    }
+    array.pop();
+    return array;
+  }
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
