7ee7b6cf919e994fae4554346cedd4454ecc91ba
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-24 12:58:17 +0800

    Add solution to 4_permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,18 @@
-const permutations = function() {
-  
+const permutations = function (array) {
+  let totalPermutations = [];
+  if (array.length === 0) {
+    totalPermutations.push([]);
+  } else if (array.length === 1) {
+    totalPermutations.push(array);
+  } else {
+    for (const value of array) {
+        for (const permutation of permutations(array.filter(item => item !== value))) {
+            totalPermutations.push([value].concat(permutation));
+        }
+    }
+  }
+  return totalPermutations;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
